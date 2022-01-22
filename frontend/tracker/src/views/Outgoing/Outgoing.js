6978f9ef0a4d2ab48c12505ecebce86bd494bf16
Richard Arthur <richardarthur228@gmail.com>
2022-01-22 21:32:59 +0000

    updated outgoing

--- frontend/tracker/src/views/Outgoing/Outgoing.js
+++ frontend/tracker/src/views/Outgoing/Outgoing.js
@@ -1,65 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Outgoing.css";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 
 import { Link } from "react-router-dom";
-import { useStateValue } from "../../store/StateProvider";
 import Folder from "../../components/DocIcon/Folder";
 import File from "../../components/DocIcon/File";
 import EmptyPage from "../../components/EmptyPage/EmptyPage";
+import { fetchOutgoing } from "../../http/document";
+import { useStateValue } from "../../store/StateProvider";
+import LoadingBackdrop from "../../components/Loading/LoadingBackdrop";
 
 function Outgoing() {
-  const [state] = useStateValue();
+  const [store] = useStateValue();
+  const [outgoing, setOutgoing] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  //   const data = state.outgoing;
+  const _fetchOutgoing = async () => {
+    const res = await fetchOutgoing(store.token);
+    const data = res.data;
+    setOutgoing(data);
+  };
 
-  //   if (data.length === 0) {
-  //     return <EmptyPage type="Outgoing" />;
-  //   }
-
-  //   const handleData = (data) => {
-  //     console.log("working", data);
-  //   };
+  useEffect(() => {
+    _fetchOutgoing();
+    setLoading(false);
+  }, []);
 
   return (
-    <div className="outgoing">
-      <div className="outgoing__container">
-        <h2 className="outgoing__header">Pending</h2>
-        <div className="outgoing__content">
-          {/* {data && (
-            <div className="outgoing__items">
-              {data.map((item) => {
-                if (item.related_document.length > 0) {
-                  return (
-                    <Folder doc={item} key={item.document.id} type="outgoing" />
-                  );
-                } else {
-                  return (
-                    <File doc={item} key={item.document.id} type="outgoing" />
-                  );
-                }
-              })}
+    <>
+      {!loading ? (
+        <div className="outgoing">
+          <div className="outgoing__container">
+            <h2 className="outgoing__header">Pending</h2>
+            <div className="outgoing__content">
+              <div className="outgoing__items">
+                {outgoing.map((item) => {
+                  if (item.related_document.length > 0) {
+                    return (
+                      <Folder
+                        doc={item}
+                        key={item.document.id}
+                        type="outgoing"
+                      />
+                    );
+                  } else {
+                    return (
+                      <File doc={item} key={item.document.id} type="outgoing" />
+                    );
+                  }
+                })}
+              </div>
+              <Link to="/dashboard/add-document">
+                <Fab
+                  size="medium"
+                  sx={{
+                    backgroundColor: "#582F08",
+                    color: "#E3BC97",
+                    position: "absolute",
+                    right: "10px",
+                    bottom: "0px",
+                  }}
+                  aria-label="add"
+                >
+                  <AddIcon />
+                </Fab>
+              </Link>
             </div>
-          )} */}
-          <Link to="/dashboard/add-document">
-            <Fab
-              size="medium"
-              sx={{
-                backgroundColor: "#582F08",
-                color: "#E3BC97",
-                position: "absolute",
-                right: "10px",
-                bottom: "0px",
-              }}
-              aria-label="add"
-            >
-              <AddIcon />
-            </Fab>
-          </Link>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <LoadingBackdrop loading={loading} />
+      )}
+    </>
   );
 }
 
