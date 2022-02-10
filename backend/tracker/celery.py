51629f7ad80767eef98f5f6729d47d3e4aebb470
Richard Arthur <richardarthur228@gmail.com>
2022-02-10 07:35:47 +0000

    created celery file

---
+++ backend/tracker/celery.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracker.settings')
+
+app = Celery('tracker')
+
+app.config_from_object(settings, namespace='CELERY')
+
+
+# app.conf.beat.schedule = {}
+
+app.autodiscover_tasks()
