67872c0fbee3e449710c6966fe797955a3ba6025
Richard Arthur <richardarthur228@gmail.com>
2022-02-10 07:36:58 +0000

    created celery task file

---
+++ backend/api/tasks.py
@@ -0,0 +1,28 @@
+from msilib.schema import Error
+from celery import shared_task
+
+from celery import Celery, states
+from celery.exceptions import Ignore
+
+from . import models
+
+
+@shared_task(bind=True)
+def expire_document(self, data):
+    print(data)
+    try:
+        activated_document = models.ActivateDocument.objects.filter(
+            id=int(data))
+        if len(activated_document) > 0:
+            activated_document = activated_document.first()
+            activated_document.expired = True
+            activated_document.save()
+            print(activated_document.expired)
+        else:
+            self.update_state(state='FAILURE_len', meta={'exe': 'Not Found'})
+    except:
+        self.update_state(state='FAILURE_', meta={'exe': 'Not Found'})
+        # raise Ignore()
+
+    # self.update_state(state='SUCCESS', meta={'exe': 'completed'})
+    return 'Done'
