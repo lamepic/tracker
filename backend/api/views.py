21410ae917a3a79c5a0352ad59d2c99533b41f49
Richard Arthur <richardarthur228@gmail.com>
2022-02-24 11:11:13 +0000

    small changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,10 @@
 import json
 from datetime import datetime
 from django.dispatch import receiver
-from django.shortcuts import get_object_or_404
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.db.models import Q
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 
 import random
 import string
